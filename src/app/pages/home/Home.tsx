b3809ff05bcb7b4206cfb123fd9423de3b43dd65
Rustem Muzipov <isu7661@gmail.com>
2022-03-30 10:25:14 +0300

    Добавил карту

--- src/app/pages/home/Home.tsx
+++ src/app/pages/home/Home.tsx
@@ -3,16 +3,29 @@ import {Slider} from "../../components/slider/Slider";
 import {Cards} from "../../components/cards/Cards";
 import {Footer} from "../../components/footer/Footer";
 import {observer} from "mobx-react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 
 export const Home = () =>{
 
+    const position = [51.505, -0.09]
 
     return(
         <div>
             <Slider/>
             <Cards/>
             <Footer/>
+
+            <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={position}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
         </div>
     )
 }
